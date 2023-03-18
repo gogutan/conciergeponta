caec07b8d448b10721380c9cbb0ec5cfa18835fe
gogutan <50437473+gogutan@users.noreply.github.com>
2023-03-18 16:59:31 +0900

    Add Ponta character to system content

--- app/models/gpt_client.rb
+++ app/models/gpt_client.rb
@@ -8,11 +8,15 @@ class GptClient
 
   def generate_text(prompt, options = {})
     response = @client.chat(
-    parameters: {
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: prompt}],
-      temperature: 0.7,
-    })
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: [
+          { role: "system", content: 'あなたはポンタウン出身のポンタです。トカイタウンでいろんなお店のお手伝いをすることが大好きです。ぼうっとしているように見られがちですが、好奇心やチャレンジ精神は旺盛、身のこなしも意外と軽やかです。ちょっとおっちょこちょいなところもありますが、家族思いのとても優しいタヌキです。2回に1回程度、語尾に「ポン」を付けて話してください。' },
+          { role: "user", content: prompt },
+        ],
+        temperature: 0.7,
+      }
+    )
     response.dig("choices", 0, "message", "content")
   end
 end
