0b81f90b545436a358c95614e908fbc73047ffc4
gogutan <50437473+gogutan@users.noreply.github.com>
2023-06-13 21:23:48 +0900

    Add some messages

--- app/models/discord_bot.rb
+++ app/models/discord_bot.rb
@@ -9,13 +9,41 @@ class DiscordBot
   end
 
   def settings
-    @bot.message(start_with: '/add') do |event|
-      pp event
-      event.respond 'Hello, world!'
+    @bot.message(start_with: 'add ') do |event|
+      body = event.message.content.sub('add ', '')
+      Memo.create(service_type: :discord_group, uid: event.channel.id, body: body)
+      event.respond "メモに #{body} を追加したポン"
+    rescue ActiveRecord::RecordNotUnique => e
+      puts e.inspect
+      event.respond "既に #{body} は存在するポン"
     end
 
-    # sample
-    # limit
-    # all
-    # destroy など足す
+    @bot.message(start_with: 'sample') do |event|
+      sample = Memo.discord_group
+                   .where(uid: event.channel.id)
+                   .sample
+      event.respond "ポン！\n#{sample.body}"
+    end
+
+    @bot.message(start_with: 'limit') do |event|
+      number = event.message.content.sub('limit ', '').to_i
+      memos = Memo.discord_group
+                  .where(uid: event.channel.id)
+                  .shuffle
+                  .take(number)
+      pons = 'ポン' * memos.size
+      memos_message = memos.map { |memo| "\n#{memo.body}" }.join
+      event.respond "#{pons}！#{memos_message}"
+    end
+
+    @bot.message(start_with: 'destroy') do |event|
+      body = event.message.content.sub('destroy ', '')
+      memo = Memo.discord_group
+                 .where(uid: event.channel.id)
+                 .find_by(body: body)
+      if memo&.destroy
+        event.respond "メモから #{body} を削除したポン"
+      end
+    end
+  end
 end
